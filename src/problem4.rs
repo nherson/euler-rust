c77cdd54ea3012d844148293fb28c116f2149960
Nicholas Herson <nicholas.herson@gmail.com>
2016-11-22 11:46:59 -0800

    improve comments for problem 4

--- src/problem4.rs
+++ src/problem4.rs
@@ -9,6 +9,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 pub fn run() {
     println!("Problem 4!");
 
+    // Must be mutable since it gets updated a lot later
     let mut max_palindrome = 0;
 
     for x in 100..1000 {
@@ -24,12 +25,19 @@ pub fn run() {
 
 // Returns true if the number is a palindrome, else false
 fn is_palindrome(n: &i32) -> bool {
+    // There are a few intermediate bindings here, because rustc complained about temporary
+    // values not having a long enough lifetime.  There is probably a way to reduce the number
+    // of bindings happening here
     let n_str = n.to_string();
     let n_chars = n_str.chars();
     let forward = n_chars;
+    // Make a `clone()` of `forward`; if you don't then calling `rev()` on `forward` is a move of `forward`
     let mut backward = forward.clone().rev();
 
     for a in forward {
+        // Iterating over two Chars (character collection) at once, so for `backward` we intermediate
+        // manually; must manually unwrap the return value of `next()` and be sure that `backward`
+        // doesn't end iteration before `forward` (shouldn't ever happen)
         let b = match backward.next() {
             Some(x) => x,
             None => panic!("Palindrome iterators are sized differently!")
